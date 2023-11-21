1b26ab5a1b4641274353d8a4310975ac623d4545
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-11-21 13:41:01 +0300

    Added a different favicon, changed page title and declared the states to be used in the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function App() 
+{
+
+  //Delcaring the states to be used in this project
+  const [cart, setCart]=useState([])
+  const [products, setProducts]= useState([])
+  const [savedItems, setSavedItems]=useState([])
+
+  useEffect(()=>
+  {
+    fetch("https://phase-2-ecommerce-project-api.onrender.com/products")
+      .then(response => response.json())
+      .then(products => console.log(products))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+    </>
   );
 }
 
