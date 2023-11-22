fc3a5b792fcfe584fde02bcbd0dc84cdc749f733
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-11-22 14:14:40 +0300

    Included a button to delete an item from the saved component

--- src/components/Saved.js
+++ src/components/Saved.js
@@ -1,6 +1,25 @@
-const Saved = () => {
+import CardBody from "./CardBody"
+
+const Saved = ({savedItems, deleteSavedItem}) => 
+{
+    
+    const itemsMap=savedItems.map(savedItem =>
+        {
+            let {id, title , image}=savedItem
+            return(
+                <div key={id} className="child-flex card">
+                    <CardBody id={id} title={title} image={image} />
+                    <button className="btn btn-outline-danger col-12" onClick={()=>deleteSavedItem(id)}>Remove item</button>
+                </div>
+            )
+        })
     return (  
-        <h1>Saved items</h1>
+        <>
+            <h1>Saved items</h1>
+            <div className="parent-flex">
+                {itemsMap}
+            </div>
+        </>
     );
 }
  
